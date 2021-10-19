c7007fc03da2c2021d53e3df5491b8efa78b8b69
Linda <lindathehuang@gmail.com>
2021-10-18 19:27:32 -0700

    Added preloader

--- stylescripts/script.js
+++ stylescripts/script.js
@@ -17,6 +17,8 @@ const pageDesc = [
 let navLinks = [];
 
 $(document).ready(function(){
+    $("#preloader").delay(1500).fadeOut("slow")
+    
     for(i=0; i<pageID.length; i++) {
         let setupID = "#"+pageID[i];
         let setupDirectory = "pages/" + pageID[i] + ".html";
